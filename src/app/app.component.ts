efcf90fc69a905ee299a2482a6f4ad2d6ce08b31
Mark <mdirksma@gmail.com>
2019-10-31 15:01:26 +0100

    updates UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'innovatiedag-angular';
+  version = "unknown";
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.httpClient.get(environment.apiUrl).subscribe((response : any) => {
+      this.version = response.version;
+    });
+  }
 }
